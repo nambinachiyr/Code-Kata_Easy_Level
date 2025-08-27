ddc52bd30f7f70580e8974408a6c03848e132f56
Nambi Nachiyar <nambi2731@gmail.com>
2025-08-27 17:40:41 +0530

    Power of 2 next number

--- OES.js
+++ OES.js
@@ -14,11 +14,30 @@ inp.on("line", (data) => {
 });
 
 inp.on("close", () => {
-  function prime(arr){
-    let N = arr[0]
-    if(N%2===0 || N%3===0 || N%5===0 ||N%7===0 || N%9===0){
-      return 1
-    }else return 0
-   }prime(userInput)?console.log("no"):console.log("yes")
+  // This Logic is fine but notgood
+  // function grePower(arr){
+  //   let N = Number(arr[0])
+  //   let power = Math.floor(Math.log2(N))
+  //   let result = 2**power
+  //   let num = N;
+  //   console.log(power)
+  //   if(result===num){
+  //     console.log("yes")
+  //     return 2**(power+1)
+  //   }else console.log("This is not a list of 2 powers")
+  //  }console.log(grePower(userInput))
   
+  function power2(arr){
+    let N = Number(arr[0]);
+   if((N&(N-1))===0){
+    return N*2
+   }
+   else{
+    let power = Math.ceil(Math.log2(N))
+  //  console.log(2**power)
+   return 2**power
+   }
+  }
+  console.log(power2(userInput))
+
 });
