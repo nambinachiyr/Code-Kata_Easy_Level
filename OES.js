4bc84c332fe9b30d22cd3e1a4486aa7a9c639140
Nambi Nachiyar <nambi2731@gmail.com>
2025-08-28 22:39:55 +0530

    reverse_num

--- OES.js
+++ OES.js
@@ -16,31 +16,30 @@ inp.on("line", (data) => {
 });
 /*
 Problem Statement:
-Write a program to calculate the total surface area and volume of cuboid.
+Given a number N, print its reverse.
 Input Description:
-Input contains three space separated positive integers L, B, H denoting the length, width and height of cuboid respectively.
-Output Description:
-The output should be the total surface area and volume of the cuboid, separated by a space.
+Input Size : n <= 1000
 Sample Input:
-1 2 3
+10
 Sample Output:
-22 6
+1
 */
 
 inp.on("close", () => {
-  let arr1 = userInput[0].split(" ")
-   let l = arr1[0]
-   let b = arr1[1]
-   let h = arr1[2]
- function surface_Area(l,b,h){
-   let area = 2*(l*b+b*h+h*l)
-   return (area)   
- }
- function volume_of_cuboid(l,b,h){
-   let volume = l*b*h
-   return (volume)
- }
-
-console.log(surface_Area(l,b,h), volume_of_cuboid(l,b,h))
+   let Cut_Zero = Number(userInput[0])
+   let reverse_Arr = [];
+   while(Cut_Zero>0){
+    let sum = 0
+     sum = sum*10+Cut_Zero%10;
+     Cut_Zero = Math.floor(Cut_Zero/10)
+     reverse_Arr.push(sum)
+     if(reverse_Arr[0]===0){
+      reverse_Arr.pop()
+  }
+   }
+  
+  //  console.log(Cut_Zero)
+  
+   console.log(reverse_Arr.join(""))
 })
 // ;Rate 4/5 nearest greater num
