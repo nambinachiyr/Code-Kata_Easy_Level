6736f0a8e2d74e9b012bb43f973badc2361b967e
Nambi Nachiyar <nambi2731@gmail.com>
2025-08-28 23:27:03 +0530

    ascending count

--- OES.js
+++ OES.js
@@ -2,6 +2,7 @@
 
 
 const { constants } = require("buffer");
+const { count } = require("console");
 const readline = require("readline");
 const { isNumberObject } = require("util/types");
 
@@ -16,30 +17,40 @@ inp.on("line", (data) => {
 });
 /*
 Problem Statement:
-Given a number N, print its reverse.
-Input Description:
-Input Size : n <= 1000
+Given a number N followed by N elements which can be arranged in ascending order with maximum one element update. Print the index of the element which has to be changed else print '-1' if the updation not neccassary or if the given input needs more than one update to form ascending order.
 Sample Input:
-10
+7
+1 2 4 3 5 6 8
+5
+1 10 3 14 5
 Sample Output:
-1
+2
+-1
 */
 
 inp.on("close", () => {
-   let Cut_Zero = Number(userInput[0])
-   let reverse_Arr = [];
-   while(Cut_Zero>0){
-    let sum = 0
-     sum = sum*10+Cut_Zero%10;
-     Cut_Zero = Math.floor(Cut_Zero/10)
-     reverse_Arr.push(sum)
-     if(reverse_Arr[0]===0){
-      reverse_Arr.pop()
+  let length = Number(userInput[0])
+  let elements = userInput[1].split(" ")
+  let newEle = elements.map((ele,i)=>{
+     return Number(ele)
+  })
+  let count = 0;
+  let sort_index = -1
+  let index = 0;
+  let result = "";
+  if(length===newEle.length){
+    for(let i = 0;i<length-1;i++){
+      if(newEle[i]<newEle[i+1]){
+      }else{
+        count++;
+        index = i
+      }
+       result = count==1?index:sort_index
+    }
   }
-   }
-  
-  //  console.log(Cut_Zero)
-  
-   console.log(reverse_Arr.join(""))
+  // console.log(sort_index)
+  // console.log(count)
+  // console.log(index)
+  console.log(result)
 })
 // ;Rate 4/5 nearest greater num
