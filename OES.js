f78f6b9d36f09bd66de3c3cab07c758c6e770225
Nambi Nachiyar <nambi2731@gmail.com>
2025-09-01 23:08:12 +0530

    Find SecondSmallest Element

--- OES.js
+++ OES.js
@@ -15,74 +15,45 @@ const userInput = [];
 inp.on("line", (data) => {
   userInput.push(data);
 });
-/*28.
+/*
+33.
 Problem Statement:
-Count the number of digits of a given number N.Size of the integer ranges from 1<N<100000000
-
-Sample Input:
-548
-
-Sample Output:
-3
-29.
-Problem Statement:
-Given a string S, print it without using semicolon in your program.
-Sample Input:
-hello world
-Sample Output:
-hello world
-30.
-Problem Statement:
-Given a number N, find the nearest greater multiple of 10.
+Given a number N followed by N elements, find the second smallest element.If it cannot be found then print -1
 Input Description:
-Input Size : N <= 10000
+Input Size : N <= 100000 (ie do it in O(log n) time complexity)
 Sample Input:
-3
+5
+1 2 3 4 5
 Sample Output:
-10
-31.
-Problem Statement:
-Given 2 numbers N,M. Find their difference and check whether it is even or odd.
-Sample Input:
-5 5
-Sample Output:
-even
-32.
-Problem Statement:
-Write a program to print the sum of the first K natural numbers.
-Input Description:
-Input Size : n <= 100000
-Sample Input:
-3
-Sample Output:
-6
+2
 */
 
 inp.on("close", () => {
-    // 28
-    // let num = userInput[0].split("")
-    // console.log(num.length)
-    // 29
-    // let num = userInput[0]
-    // console.log(num)
-    // 30
-    // let user = Number(userInput[0])
-    // let multi = user%10
-    // let com = 10-multi
-    // let num = user+com
-    // console.log(num)
-    // console.log(multi)
-    // 31.
-    // let nums = userInput[0].split(" ").map(Number)
-    // let firstNum = nums[0]
-    // let secondNum = nums[1]
-    // let diff = firstNum-secondNum;
-    // let difNum = diff<0?-1*diff:diff
-    // console.log(difNum%2===0?"even":"odd")
-    // 32.
-    // let user = Number(userInput[0])
-    // let naturalNum = (user*(user+1))/2 //n(n+1)/2
-    // console.log(naturalNum)
-    
+    let length = Number(userInput[0])
+    let num = userInput[1].split(" ").map(Number)
+    // let min = num[0]
+    // let max = num[1]
+    // // Thorugh Looping -> Method one
+    // for(let i =0; i<length; i++){ 
+    //   // console.log(i)     
+    //   if(num[i]<min){
+    //     max = min
+    //     min = num[i]
+    //   }
+    //   else if(min<num[i] && max>num[i]){
+    //     max = num[i]        
+    //   }      
+    // }max!==min?console.log(max):console.log(-1)
+    //got 4/5
+    // let sort = num.sort()
+    // console.log(sort[1])   
+   let unique = [...new Set(num)]
+   if(unique.length<2){
+    console.log(-1)
+   }
+   else{
+    unique.sort((a,b)=>a-b)
+    console.log(unique[1])
+   }
+  //  nice perfect
 })
-// ;Rate 4/5 nearest greater num
