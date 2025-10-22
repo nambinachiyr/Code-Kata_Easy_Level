e81beddc23b763ce7a77b8ec8404bce9698af8e3
Nambi Nachiyar <nambi2731@gmail.com>
2025-10-23 01:56:15 +0530

    56. Find Different Number in Series

--- OES.js
+++ OES.js
@@ -1,25 +1,22 @@
 /*
-57. Max Length Increasing Subarray
+56. Find Different Number in Series
 
 Problem Statement:
-Given an array of N elements,find the maximum length of increasing continuous sub-array.If it is not found print '-1'.
+Given an array of N elements which follows either even number or odd number series.There may exists at maximum 1 even number in the odd series or 1 odd number in the even series.Find the different number if exists otherwise print '-1'?
 
 
 Input Description:
-Input Size : N <= 100000
-
-
-Output Description:
-The maximum length of the increasing continuous sub-array, or '-1' if not found.
+Input Size : |N| <= 100000
 
 
 Sample Input:
 5
-1 2 3 2 1
+1 3 4 5 7
 
 
 Sample Output:
-3
+4
+
 */
 
 
@@ -36,22 +33,34 @@ inp.on("line", (data) => {
 });
 
 const difference = (arr1)=>{
-  let maxLen = 1
-  let curLen = 1
-  for(let i = 1;i<arr1.length;i++){
-    if(arr1[i]>arr1[i-1]){
-      curLen++
+  let oddCount = 0
+  let evenCount = 0
+  let oddIndex = 0
+  let evenIndex = 0
+  for(let i = 0;i<arr1.length;i++){
+    if(arr1[i]%2!==0){
+      oddCount++
+      (oddCount===1)&&(oddIndex = arr1[i])
     }else{
-       if(maxLen<curLen){
-        maxLen = curLen
-      }curLen = 1
+      evenCount++
+      (evenCount===1)&&(evenIndex = arr1[i])
     }
   }
-  if(maxLen<curLen){
-    maxLen = curLen
+  // If all odd or even elements
+  if(oddCount===arr1.length||evenCount===arr1.length){
+    return console.log(-1)
+  }   
+  // One odd number 
+  else if(oddCount===1){
+    return console.log(oddIndex)
   }
-  if(maxLen<=1) maxLen = -1;
-  console.log(maxLen)
+  // One even number
+  else if(evenCount===1){
+    return console.log(evenIndex)
+  }//Mutiple odd or even
+  else{
+    return console.log(-1)
+  }
 
 }
 
