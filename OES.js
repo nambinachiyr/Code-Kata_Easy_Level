af39d4f2fba55f653c8ae8b1ef3a5a4e046ee366
Nambi Nachiyar <nambi2731@gmail.com>
2025-10-23 02:41:32 +0530

    55. Counting Distinct Pairs1___Check the set{}

--- OES.js
+++ OES.js
@@ -1,21 +1,25 @@
 /*
-56. Find Different Number in Series
+55. Counting Distinct Pairs
 
 Problem Statement:
-Given an array of N elements which follows either even number or odd number series.There may exists at maximum 1 even number in the odd series or 1 odd number in the even series.Find the different number if exists otherwise print '-1'?
+Given an array A of N elements, count the number of distinct pairs (i,j) such that i < j and A[i] < A[j].If no such pairs can be made print -1
 
 
 Input Description:
-Input Size : |N| <= 100000
+The input consists of an integer N, representing the number of elements, followed by N space-separated integers representing the elements of array A.
+
+
+Output Description:
+The output is a single integer representing the count of distinct pairs (i,j) such that i < j and A[i] < A[j]. If no such pairs can be made, print -1.
 
 
 Sample Input:
 5
-1 3 4 5 7
+1 2 3 4 5
 
 
 Sample Output:
-4
+10
 
 */
 
@@ -33,35 +37,18 @@ inp.on("line", (data) => {
 });
 
 const difference = (arr1)=>{
-  let oddCount = 0
-  let evenCount = 0
-  let oddIndex = 0
-  let evenIndex = 0
+  let count = []
   for(let i = 0;i<arr1.length;i++){
-    if(arr1[i]%2!==0){
-      oddCount++
-      (oddCount===1)&&(oddIndex = arr1[i])
-    }else{
-      evenCount++
-      (evenCount===1)&&(evenIndex = arr1[i])
+    for(let j = i+1;j<arr1.length;j++){
+      if(arr1[i]<arr1[j]){
+        let newArr = [arr1[i],arr1[j]]
+        count.push(newArr)
+      }
     }
-  }
-  // If all odd or even elements
-  if(oddCount===arr1.length||evenCount===arr1.length){
-    return console.log(-1)
-  }   
-  // One odd number 
-  else if(oddCount===1){
-    return console.log(oddIndex)
-  }
-  // One even number
-  else if(evenCount===1){
-    return console.log(evenIndex)
-  }//Mutiple odd or even
-  else{
-    return console.log(-1)
-  }
 
+  }if(count.length===0) return console.log(-1);
+  const setArr = new Set(count.map(pair=>pair.sort((a,b)=>a-b).join(',')))
+  console.log(setArr.size)
 }
 
 inp.on("close", () => {
