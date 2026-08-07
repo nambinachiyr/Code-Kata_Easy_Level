390127f931532fffa3e2104dbec547640d16473d
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-07 22:41:56 +0530

    37. First Occurrence of Character

--- OES.js
+++ OES.js
@@ -1,20 +1,23 @@
 /*
-35. String Length Without Functions
+37. First Occurrence of Character
 
-Geekoin30
-Easy
+Geekoin50
+Medium
 Topics
 Problem Statement:
-Given a string S, find its length(including the spaces)without using any pre-defined functions.
+Given a string 'S' and a character 'K', find at what position the character 'K' occurs for the first time in 'S'.(Assume the index of string starts at 1).If the character is not found in 'S' then print -1
+
+
+Input Description:
+Input Size : |s| <= 100000
 
 
 Sample Input:
-codekata
+codekata a
 
 
 Sample Output:
-8
-
+6
 
 */
 
@@ -31,9 +34,24 @@ inp.on("line", (data) => {
   userInput.push(data.trim());
 });
 
+function result(string,letter){
+    for(let s in string){
+      // console.log(letter)
+      if(string[s]===letter){
+        return console.log(Number(s)+1)
+        break;
+      }
+    }
+    console.log(-1)
+}
+
 inp.on("close", () => {
-  console.log(userInput) 
-  const sentance = userInput[0].split('')
-  console.log(sentance.length)  
+  // console.log(userInput) 
+  const arr = userInput[0].split(' ')
+  const string = arr[0]
+  const letter = arr[1]
+  // console.log(string,"S")
+  // console.log(letter,"L") 
+  result(string,letter)
   
 });
