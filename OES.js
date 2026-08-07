52e3a400ae51fdcf4ca2adae259fcb466bcd50e9
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-07 22:23:41 +0530

    35. String Length Without Functions

--- OES.js
+++ OES.js
@@ -1,29 +1,19 @@
 /*
-39. Count String Occurrences
+35. String Length Without Functions
 
 Geekoin30
 Easy
 Topics
-Solved!
 Problem Statement:
-Given a sentence and string S, find how many times S occurs in the given sentence.If S is not found in the sentence print -1
-
-
-Input Description:
-Input Size : |sentence| <= 1000000(complexity O(n)).
-
-
-Output Description:
-The output is the number of times S occurs in the given sentence, or -1 if S is not found.
+Given a string S, find its length(including the spaces)without using any pre-defined functions.
 
 
 Sample Input:
-I enjoy doing codekata
 codekata
 
 
 Sample Output:
-1
+8
 
 
 */
@@ -42,13 +32,8 @@ inp.on("line", (data) => {
 });
 
 inp.on("close", () => {
-  // console.log(userInput) //[ 'code code code', 'learn' ]
-  const sentance = userInput[0].split(' ')
-   const str = userInput[1].split(' ')
-  // console.log(sentance,str)   //[ 'code', 'code', 'code' ] [ 'learn' ]
-  const filtered = sentance.filter(s=>s===str[0])
-  // console.log(filtered)  //[]
-  filtered.length!==0?console.log(filtered.length):console.log(-1)  //-1
-
+  console.log(userInput) 
+  const sentance = userInput[0].split('')
+  console.log(sentance.length)  
   
 });
