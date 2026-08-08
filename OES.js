e4762d04d7d67ed3360983a4bdcde42b68219595
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-08 22:54:16 +0530

    46. Descending Elements Below N

--- OES.js
+++ OES.js
@@ -1,23 +1,30 @@
 /*
-45. Sorted Elements Less Than K
+46. Descending Elements Below N
 
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given 2 numbers N,K followed by N elements print all the elements lesser than K in sorted order.If the elements could not be found print -1
+Given a number N and an array of N elements, print all elements lesser than N in descending order. If no element found print -1.
 
 
 Input Description:
-Input Size : N <= 100000
+The input consists of a number N, and an array of N elements. N is between 1 and 10000 (inclusive).
+
+
+Output Description:
+Print all elements from the array that are lesser than N, in descending order. If no such elements are found, print -1.
 
 
 Sample Input:
-5 3 1 2 1 4 1
+5
+2 14 15 14 3
 
 
 Sample Output:
-1 1 1 2
+3 2
+
+
 */
 
 
@@ -39,12 +46,12 @@ function result(arr,K){
  
 
 // console.log(sameNum)
-sameNum.length===0?console.log(-1):console.log(sameNum.sort().join(' '))
+sameNum.length===0?console.log(-1):console.log(sameNum.sort().reverse().join(' '))
 }
 
 inp.on("close", () => {
   // console.log(userInput) 
-  const [N, K] = userInput[0].split(' ').map(n=>Number(n))
+  const K = Number(userInput[0])
   const arr = userInput[1].split(' ').map(n=>Number(n))
 
   result(arr,K)
