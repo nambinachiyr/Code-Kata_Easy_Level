da6b0ba4dc310f52f84e13630d2a3d61063aa2e9
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-08 21:42:55 +0530

    43. Minimum Odd Quotient Factor

--- OES.js
+++ OES.js
@@ -1,28 +1,29 @@
 /*
-42. Pair Sum Check
+43. Minimum Odd Quotient Factor
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given 2 numbers N,X and an array of N elements, check if there exists any 2 numbers in the array with sum equal to X.If found print 'yes' otherwise print 'no'
+Given number N, find the minimum factor which yeilds odd number as the quotient.
 
 
 Input Description:
-The input consists of two numbers N and X, and an array of N elements. N and X are up to 100000.
+The input consists of a single integer N, where N <= 100000.
 
 
 Output Description:
-Print 'yes' if two numbers with sum equal to X are found in the array, otherwise print 'no'.
+The output is the minimum factor of N that yields an odd number as the quotient.
 
 
 Sample Input:
-4 4
-2 2 0 0
+9
 
 
 Sample Output:
-yes
+1
+
+
 
 
 
@@ -41,29 +42,28 @@ inp.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(arr,X){
-  let count = false
-  for(let i=0;i<arr.length;i++){    
-    for(let j=i+1;j<arr.length;j++){
-      if(arr[i]+arr[j] === X){
-        // console.log(arr[i],'-',arr[j])
-        //  console.log("yes")
-         count = true
-         break;
-      }
-    }
-    if(count){
-      break
+function result(N){
+  const factors = []
+  for(let i = 1 ;i<=N;i++){
+    if(N % i === 0){
+       factors.push(i)
     }
   }
-  count ?console.log("yes"): console.log("no")
+  // console.log(factors)
+  const result = factors.filter(f=>{
+    let que = N/f
+    if(que %2 !==0){
+      return f
+    }
+  })
+  // console.log(result)
+  result.length===0?console.log(-1):console.log(result[0])
 }
 
 inp.on("close", () => {
-  console.log(userInput) 
-  const [N,X] = userInput[0].split(' ').map(Num=>Number(Num))
-  const arr = userInput[1].split(' ').map(Num=>Number(Num))
-  console.log(arr)
-  result(arr,X)
+  // console.log(userInput) 
+  let N = Number(userInput[0])
+  result(N)
+ 
   
 });
