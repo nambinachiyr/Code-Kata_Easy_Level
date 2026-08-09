1a01fc65e3b01879f8e7563c47387b0ca8ffd65d
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-09 13:58:14 +0530

    48. Factorial of a Number-2

--- OES.js
+++ OES.js
@@ -1,32 +1,27 @@
 /*
-47. Numbers Repeated K Times
+48. Factorial of a Number-2
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given 2 numbers N,K and an array of N elements, 
-print the number(s) that has been repeated K times.
-Print them in ascending order if there are more than one number to be printed.
-If no element satisfies the pattern then print -1
+Given a number N, find the factorial of N.
 
 
 Input Description:
-The input consists of two integers N and K, followed by an array of N elements. N and K are up to 100000.
+The input consists of a single integer N, constrained by 1 <= N <= 25.
 
 
 Output Description:
-Print the numbers that have been repeated K times in ascending order. If no such element exists, print -1.
+The output is the calculated factorial of N.
 
 
 Sample Input:
-5 2
-1 2 4 1 2
+5
 
 
 Sample Output:
-1 2
-
+120
 
 */
 
@@ -43,32 +38,25 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(arr,K){
-  const sameElement = []
- for(let i =0;i<arr.length;i++){
-  let count = 0
-  for(let j =i; j<arr.length ;j++){
-  
-     if(arr[i]===arr[j]){
-      
-      count = count+1;
-      if(count===K){
-      
-        sameElement.push(arr[i])
-        break;
-      }
-     }
-  }
- }
-sameElement.length!==0?console.log(sameElement.sort().join(' ')):console.log(-1)
+function result(N){
+//   const sameElement = []
+//  for(let i =1;i<=N;i++){
+//    sameElement.push(i)     
+//  } //Using Loop to create the arr with N value
+
+const sameElement = Array.from({length:N},(_,i)=>i+1) //Array Function to create the N value 
+
+ const sum = sameElement.reduce((acc,cur)=>{return acc*cur},1)
+ console.log(sum)
+ 
 }
 
 rl.on("close", () => {
   // console.log(userInput) 
-  const [N, K] = userInput[0].split(' ').map(n=>Number(n))
-  const arr = userInput[1].split(' ').map(n=>Number(n))
+  const N = Number(userInput[0])
+  
 
-  result(arr,K)
+  result(N)
  
   
 });
