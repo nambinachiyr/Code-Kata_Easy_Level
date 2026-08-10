7c4c316acab9664d28199737649cd3ae8d542213
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-10 19:22:06 +0530

    49. Longest Repeating Sequence

--- OES.js
+++ OES.js
@@ -1,27 +1,24 @@
 /*
-48. Factorial of a Number-2
+49. Longest Repeating Sequence
 
 Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a number N, find the factorial of N.
+Given a number N and an array of N elements, find the length of the longest repeating sequence of the elements.If no such sequence is found print -1
 
 
 Input Description:
-The input consists of a single integer N, constrained by 1 <= N <= 25.
-
-
-Output Description:
-The output is the calculated factorial of N.
+The input consists of an integer N (where N <= 100000) and an array of N elements.
 
 
 Sample Input:
-5
+8
+1 2 2 2 3 4 5 6
 
 
 Sample Output:
-120
+3
 
 */
 
@@ -38,25 +35,30 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(N){
-//   const sameElement = []
-//  for(let i =1;i<=N;i++){
-//    sameElement.push(i)     
-//  } //Using Loop to create the arr with N value
+function result(N,arr){
+  let maxCount = 1
+  let current =1 ;
 
-const sameElement = Array.from({length:N},(_,i)=>i+1) //Array Function to create the N value 
-
- const sum = sameElement.reduce((acc,cur)=>{return acc*cur},1)
- console.log(sum)
- 
+  for(let i = 1 ;i<N;i++){
+   if(arr[i]===arr[i-1]){
+    // console.log("cur " ,'=',arr[i])
+     current ++
+   }else{
+    count = 1 ;
+   }if(current>maxCount){
+    maxCount = current
+   }
+ }
+ maxCount===1?console.log(-1):console.log(maxCount)
 }
 
 rl.on("close", () => {
   // console.log(userInput) 
-  const N = Number(userInput[0])
+  const N = userInput[0].split(' ').map(n=>Number(n))
+  const arr = userInput[1].split(' ').map(n=>Number(n))
   
 
-  result(N)
+  result(N,arr)
  
   
 });
