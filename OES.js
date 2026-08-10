9cf3c82a52867e3f66a84a1a3ca985d9b3b206bc
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-10 19:39:18 +0530

    50. Delete Last K Array Elements

--- OES.js
+++ OES.js
@@ -1,24 +1,27 @@
 /*
-49. Longest Repeating Sequence
+50. Delete Last K Array Elements
 
 Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a number N and an array of N elements, find the length of the longest repeating sequence of the elements.If no such sequence is found print -1
+Given 2 numbers N,K print the array after deleting the last K elements.
 
 
 Input Description:
-The input consists of an integer N (where N <= 100000) and an array of N elements.
+N,K <= 100000
+
+
+Output Description:
+The array after deleting the last K elements.
 
 
 Sample Input:
-8
-1 2 2 2 3 4 5 6
-
+5 4
+1 2 3 4 5
 
 Sample Output:
-3
+1
 
 */
 
@@ -35,30 +38,20 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(N,arr){
-  let maxCount = 1
-  let current =1 ;
-
-  for(let i = 1 ;i<N;i++){
-   if(arr[i]===arr[i-1]){
-    // console.log("cur " ,'=',arr[i])
-     current ++
-   }else{
-    count = 1 ;
-   }if(current>maxCount){
-    maxCount = current
-   }
- }
- maxCount===1?console.log(-1):console.log(maxCount)
+function result(N,arr,K){
+  const length = Math.abs(arr.length-K)
+  
+ arr.length = length
+ console.log(arr.join(' '))
 }
 
 rl.on("close", () => {
   // console.log(userInput) 
-  const N = userInput[0].split(' ').map(n=>Number(n))
+  const [N,K] = userInput[0].split(' ').map(n=>Number(n))
   const arr = userInput[1].split(' ').map(n=>Number(n))
   
 
-  result(N,arr)
+  result(N,arr,K)
  
   
 });
