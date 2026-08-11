42e2e8945607de7c3aba1b8b1a7fd991d375366d
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-11 09:54:04 +0530

    61. Max Element using Bitwise AND

--- OES.js
+++ OES.js
@@ -1,32 +1,27 @@
 /*
-55. Counting Distinct Pairs
+61. Max Element using Bitwise AND
 
-Geekoin50
+Geekoin40
 Medium
-Topics
-Solved!
+Company
 Problem Statement:
-Given an array A of N elements, count the number of distinct pairs (i,j) such that i < j and A[i] < A[j].If no such pairs can be made print -1
+Given a number N and an array of N elements, find the maximum of the elements (using Bitwise AND) and print the output.
 
 
 Input Description:
-The input consists of an integer N, representing the number of elements, followed by N space-separated integers representing the elements of array A.
-
-
-Output Description:
-The output is a single integer representing the count of distinct pairs (i,j) such that i < j and A[i] < A[j]. If no such pairs can be made, print -1.
+Input Size N <= 100000
 
 
 Sample Input:
-5
-1 2 3 4 5
+4
+2 4 4 2
 
 
 Sample Output:
-10
-
+4
 */
-const readline = require("readline");
+
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin
@@ -39,20 +34,12 @@ rl.on("line", (data) => {
 });
 
 function result(N,arr){
-  let test = []
-  let seen = new Set()  
-  for(let i = 0 ;i<N-1;i++){
-    for(let j = i+1;j<N;j++){
-      if(i<j && arr[i]<arr[j] ){
-        let pair = `${arr[i]},${arr[j]}`
-        if(!seen.has(pair)){
-          seen.add(pair)
-          test.push([arr[i],arr[j]])
-        }
-      }
-    }
+  let test  = []
+  for(let i = 0 ;i<N;i++){
+      let result = arr[i] & arr[i]
+      test.push(result)
   } 
-  console.log(seen.size===0?-1:seen.size)
+  console.log(test.sort()[test.length-1])
  
 }
 
