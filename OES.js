b18a4bafeb1490f085cc6410facce3eefc6c91a5
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-11 08:02:44 +0530

    53. Rotated Array Rotation Count

--- OES.js
+++ OES.js
@@ -1,29 +1,22 @@
 /*
+53. Rotated Array Rotation Count
 
-52. Max of Consecutive Pairs
-
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given a number N followed by N elements for every 2 consecutive numbers print the maximum of the 2.
-
-
-Input Description:
-The input consists of an integer N, followed by N elements. N is an integer such that N <= 100000, implying an O(n) time complexity solution is expected.
-
-
-Output Description:
-The output is a space-separated sequence of the maximums of every two consecutive numbers from the input.
+Given an value 'M' follwed by array of M elements in which the elements would have been rotated for certain 'N' times from the intial array representation where all elements are arranged in ascending order.Print the 'N' or print -1 if there is no rotation made or cannot be determined.Note: 1<=N<=length of the given array.
 
 
 Sample Input:
 5
-1 1 3 0 5
+15 18 2 3 6 12
 
 
 Sample Output:
-1 3 3 5
+2
+
+
 
 
 */
@@ -41,28 +34,27 @@ rl.on("line", (data) => {
 
 function result(N,arr){
   let test = [] 
-  // console.log(arr)
+  let count = 0
   for(let i = 0;i<N-1;i++){
-    if(arr[i]>=arr[i+1]){
-      // console.log(arr[i],"=",arr[i+1])
-       test.push(arr[i])
-       
-    }
-    else{
-      test.push(arr[i+1])
+    count= count+1
+    if(arr[i]>arr[i+1]){
+     break
+    }else{
+      if(arr[i] === arr[i+1]){
+        count = 0
+      }
     }
   }
-// console.log(test)
-console.log(test.length===0?-1:test.join(' '))
+  // console.log(count)
+console.log(count===0?-1:count)
 }
 
 rl.on("close", () => {
-  const N = userInput[0].split(' ').map(n=>Number(n))
+  const N = userInput[0].split('').map(n=>Number(n))
   let arr = userInput[1].split(' ').map(n=>Number(n))
  
-  // console.log(s1,s2 ) 
 
-  result(N,arr)
+  result(N[0],arr)
  
   
 });
