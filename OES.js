e90b24ee9dca3312c25791323d160b0543dc5bf1
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-12 07:46:54 +0530

    67. Swap Odd Even String Characters

--- OES.js
+++ OES.js
@@ -1,27 +1,23 @@
 /*
-66. Isomorphic Strings
+67. Swap Odd Even String Characters
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given 2 strings,check whether it is isomorphic.If it is not isomorphic print '-1'.
+Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).
 
 
 Input Description:
-The input consists of two strings. The size of each string |s| is at most 100000.
-
-
-Output Description:
-The output is 'yes' if the strings are isomorphic, otherwise '-1'.
+Input Size : |s| <= 10000000(complexity O(n))
 
 
 Sample Input:
-aab xxy
+codekata
 
 
 Sample Output:
-yes
+ocedakat
 
 */
 
@@ -38,35 +34,22 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(str1,str2){
-  
-    function getPattern(str){
-      let test = []
-      let count = 0
-      for(let s = 0;s<str.length;s++){
-        let first = str[0]
-        if(first===str[s]){
-          count = 1
-          test.push(count)
-        }
-        else{
-          count=count+1
-          test.push(count)
-        }
-
+function result(a){
+  for(let i = 0 ;i<a.length;i++){
+    if(i%2===0 ){
+      let temp = a[i]
+      a[i] = a[i+1]
+      a[i+1]= temp
+      if(i === a.length-1){
+        break
       }
-      return test
-    } 
-    // console.log(getPattern(str1))    
-    // console.log(getPattern(str2))    
- console.log(getPattern(str1).join(' ')===getPattern(str2).join(' ')?"yes":"no")
- 
-  
+    console.log(a[i],temp,a[i+1])
+    }
+  }
+  console.log(a.join(''))
  }
 
 rl.on("close", () => {
-  let a = userInput[0].split(" ")
-  const str1 = a[0]
-  const str2 = a[1]
-  result(str1,str2)
+  let a = userInput[0].split('')  
+  result(a)
 });
