32e55f6de7cf99287a68e953c33c426b8d7ac745
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-12 08:11:51 +0530

    68. CamelCase Conversion

--- OES.js
+++ OES.js
@@ -1,23 +1,23 @@
 /*
-67. Swap Odd Even String Characters
+68. CamelCase Conversion
 
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).
+Given a string/sentence print its corresponding camelcase convention.
 
 
 Input Description:
-Input Size : |s| <= 10000000(complexity O(n))
+Input Size : |s| <= 1000000(complexity O(n))
 
 
 Sample Input:
-codekata
+guvi geeks
 
 
 Sample Output:
-ocedakat
+GuviGeeks
 
 */
 
@@ -34,22 +34,21 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(a){
-  for(let i = 0 ;i<a.length;i++){
-    if(i%2===0 ){
-      let temp = a[i]
-      a[i] = a[i+1]
-      a[i+1]= temp
-      if(i === a.length-1){
-        break
-      }
-    console.log(a[i],temp,a[i+1])
+function result(a,word1,word2){
+ for(let i = 0 ;i<a.length;i++){
+  let word =''
+     for(let j = 0;j<a[i].length;j++){
+      word = a[i][0].toUpperCase()+a[i].slice(1,)
     }
-  }
-  console.log(a.join(''))
+    // console.log(word)
+    a[i] = word
+ }
+ console.log(a.join(''))
  }
 
 rl.on("close", () => {
-  let a = userInput[0].split('')  
-  result(a)
+  let a = userInput[0].split(' ')  
+  let word1 = a[0]
+  let word2 = a[1]
+  result(a,word1,word2)
 });
