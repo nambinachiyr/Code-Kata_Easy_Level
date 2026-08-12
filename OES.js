12083a37d15ba55ea99ad1837dc8e47f8ea3cefd
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-12 09:20:04 +0530

    71. Holiday Check

--- OES.js
+++ OES.js
@@ -1,23 +1,23 @@
 /*
-70. One Character Difference
+71. Holiday Check
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given 2 strings check whether they differ exacly by one character.If yes then print 'yes' otherwise print 'no'
-
-
-Input Description:
-Input Size : |s| <= 100000(complexity O(nlogn) or O(n))
+Given a day, print 'yes' if it is a holiday otherwise print'no'.Assume that weekend days are holidays
 
 
 Sample Input:
-codekata codekate
+saturday
+monday
 
 
 Sample Output:
 yes
+no
+
+
 
 */
 
@@ -33,27 +33,49 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(a,word1,word2){
-  let count = 0
-  for(let i = 0 ;i<word1.length;i++){
-    // for(let j = 0;j<word2.length;j++){
-      if(word1[i]!==word2[i]){
-        // console.log(word1[i],'-',word2[i])
-        count++
-      }
-    // }
+function result(a){
+//  console.log(a)
+ const weekEndDays = ["saturday","sunday"]
+ const weekDays = ["monday","tuesday","wednesday","thursday","friday"]
+
+//  for(let i =0;i<a.length;i++){
+//   // console.log(i,' - i')
+//   for(let j = 0;j<weekDays.length;j++){
+//     // console.log(j,' - j')
+//     if(a[i]===weekEndDays[j]){
+//       console.log("yes")
+//       break
+//     }else if(a[i]===weekDays[j]){
+//       console.log("no")
+//       break
+//     }
+//   }
+//  }
+//   Way II
+for(let i of a){
+  let flex = false
+  for(let weekEndDay of weekEndDays){
+    for(let weekDay of weekDays){
+      if(i.toLowerCase()===weekEndDay){
+      console.log("yes")
+      flex = true
+      break
+    }else if(i.toLowerCase()===weekDay){ //or weekDays.include(i.toLowerCase())that alse we can use
+      // console.log(i)
+       console.log("no")
+       flex = true
+       break
+    }
   }
-  if(count===1){
-    console.log("yes")
-  }else{
-    console.log("no")
+  if(flex){
+    break
   }
+  }
+}
+
  }
 
 rl.on("close", () => {
-  let a = userInput[0].split(' ')
-  let word1 = a[0].split('')
-  let word2 = a[1].split("")
- 
-  result(a,word1,word2)
+  let a = userInput 
+  result(a)
 });
