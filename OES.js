e0e0db793c29288c36947839ad2e193e806e676d
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-12 13:51:51 +0530

    73. Sum of Squares of Digits

--- OES.js
+++ OES.js
@@ -1,29 +1,26 @@
 /*
-72. Cyclic Right Shift Array
+73. Sum of Squares of Digits
 
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given two numbers N,K followed by an array of N elements, print the array after doing right shift K times (in cyclic manner).
+Given a number N, print the sum of the squares of its digits.
 
 
 Input Description:
-The input consists of two numbers N, K, followed by an array of N elements. Input Size: 1 <= N, K <= 100000.
+The input consists of a number N, where 1 <= N <= 1000000000000000000.
 
 
 Output Description:
-The output is the array after performing a right cyclic shift K times.
+The output is the sum of the squares of the digits of N.
 
 
 Sample Input:
-3 2
-7 2 3
-
+19
 
 Sample Output:
- 3 7
-
+82
 */
 
 const readline = require('readline');
@@ -38,36 +35,16 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(a,N,K){
-  let count = 0
-  for(let i = 0;i<K;i++){
-  //  console.log("Running")
-    if(count!==K){
-      for(let i = 0 ;i<N-1;i++){         
-        if(count!==K){
-          let pop =  a.pop()
-          a.unshift(pop)
-          count++
-         if(count===K){
-          break;
-         }
-        }
-        console.log(count)
-        if(count === K){
-          break;
-        } 
-    }
-  }else{
-    break
+function result(nums){
+  let add = 0
+  for(let n of nums){
+    add = add+n*n
   }
-  // console.log(count)
-  }
-
- console.log(a.join(' '))
+  console.log(add)
  }
 
 rl.on("close", () => {
-  let [N,K] = userInput[0].split(" ") .map(n=>Number(n))
-  let a = userInput[1].split(' ')
-  result(a,N,K)
+  let nums = userInput[0].split("") .map(n=>Number(n))
+  
+  result(nums)
 });
