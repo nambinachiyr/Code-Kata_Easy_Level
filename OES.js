7ff96b54a5c9ab634e4fedc4420e86feed994ac9
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-13 12:53:50 +0530

    81. Collinearity of Three Points

--- OES.js
+++ OES.js
@@ -1,21 +1,21 @@
 /*
-80. Encode String by Adding 3
+81. Collinearity of Three Points
 
 Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a string S, print the encoded string by adding 3 to each character(a maps to d,b maps to e,c maps to f and so on).
-
-
-Input Description:
-Input Size : 1 <= N <= 100000
+Given 3 points check whether they lie on the same line.If they lie on the same line print 'yes' Otherwise print 'no'.
 
 
 Sample Input:
-RADAR
+0 1
+0 0
+0 2
+
+
 Sample Output:
-UDGDU
+yes
 */
 
 const { match } = require('assert');
@@ -31,22 +31,29 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(string){
-  let words = []
-  for(let i of string){
-    let num = i.charCodeAt(i)+3
-    if(num>90){
-      num = num-26
+function result(vectors,x1,x2,x3,y1,y2,y3){
+
+  if(x1===x2){
+    if(x1===x2){
+     console.log("yes")
     }
-    let newStr = String.fromCharCode(num)
-    
-     words.push(newStr)
+  }
+  else{
+    let crossProduct = (y2-y1)*(x3-x2)-(y3-y2)*(x2-x1)
+     if(crossProduct===0){
+      console.log("yes")
+     }else{
+      console.log("no")
+     }
   }
 
-  console.log(words.join(''))
+
  }
 
 rl.on("close", () => {
-  const string = userInput[0].split('')
-  result(string)
+  const vectors = userInput
+  const [x1,y1] = userInput[0].split(' ').map(n=>Number(n))
+  const [x2,y2] = userInput[1].split(' ').map(n=>Number(n))
+  const [x3,y3] = userInput[2].split(' ').map(n=>Number(n))
+  result(vectors,x1,x2,x3,y1,y2,y3)
 });
