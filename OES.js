d4ce6b09672b066daad74ed75ef9af6d80c92222
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-15 19:15:10 +0530

    100. Power of Two Check-2

--- OES.js
+++ OES.js
@@ -1,27 +1,27 @@
 /*
-99. Prime Pairs for a Number
+100. Power of Two Check-2
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a number N, print the distinct pairs formed by multiplying two prime numbers (i.e)prime x prime should yield the N.Also print the numbers in descending order.If no such pairs can be formed print '-1'.
+Given a number N, check if it is a power of 2.
 
 
 Input Description:
-The input consists of a single integer N, where 1 <= N <= 100000.
+The input consists of a number N, where 1 <= N <= 100000.
 
 
 Output Description:
-Print the distinct pairs of prime numbers (prime x prime = N) in descending order. If no such pairs can be formed, print '-1'.
+Print 'yes' if N is a power of 2, otherwise print 'no'.
 
 
 Sample Input:
-65
-
+64
 
 Sample Output:
-13 5
+yes
+
 */
 
 const readline = require('readline');
@@ -37,51 +37,29 @@ rl.on("line", (data) => {
 });
 
 function result(n){
-  let i = 2
-  let factors = []
   
-  for(let i = 2 ;i <n;i++){
-      if(Number.isInteger(n/i)){
-        let num = n/i
-        let flexArr = [num,i]
-        let itHas = factors.some(pairs=>
-          pairs[0] === flexArr[1] &&
-          pairs[1] === flexArr[0]
-        )
-      if(!itHas){
-        factors.push(flexArr)
-      }
-      
-      }
-    }
-
-    // Find the PrimeFactors
-    
-    let primeFactors = []
-    
-    for(let pair of factors){
-      let count = 0
-      for(let num of pair){
-        let isPrime = false
-        for(let j = 2 ;j<num;j++){
-          if(num % j !== 0){
-            // console.log(num)
-            isPrime = true
-            count++
-            break;
-          }else{
-            isPrime = false;
-            count = 0
-          }
-        }if(isPrime && count===2){
-           primeFactors.push(pair)
+  let isPwerOf2 = false
+  if(n===1){
+    console.log("yes")
+  }else{
+  for(let i = 1 ;n!==0;i++){
+      n = n/2
+      if(Number.isInteger(n)){
+        if(n===1){
+          isPwerOf2 = true
+          console.log("yes")
+          break
+        }else{
+        continue
         }
-        
+      }else{
+        console.log('no')
+        isPwerOf2 = false
+        break
       }
     }
-    // console.log(primeFactors)
-    
-    console.log(primeFactors.length===0?-1:primeFactors.map(n=>n.join(' ')).join(' '))
+  }
+
 }
 
 rl.on("close", () => {
