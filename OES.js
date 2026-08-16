d1c55c6a2ae50f83e4a304fcc4bd3e0c4468dc21
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-16 09:18:41 +0530

    105. String Rotation

--- OES.js
+++ OES.js
@@ -1,27 +1,23 @@
 /*
-104. Rectangle with Integer Sides
+105. String Rotation
 
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given 2 numbers P and A which are the perimeter and area of a rectangle respectively, find if there can actually be a rectangle with this perimeter and area having integer sides.If there exists such rectangle print 'yes' otherwise print 'no'.
+Given a string S and an integer K, print the string obtained by rotating the orignal string by K positions.
 
 
 Input Description:
-Input Size : 1 <= P,A <= 100000
-
-
-Output Description:
-The output is 'yes' if such a rectangle exists, otherwise 'no'.
+Input Size : 1 <= N, K <= 100000
 
 
 Sample Input:
-20 25
+katacode 4
 
 
 Sample Output:
-yes
+codekata
 
 */
 
@@ -37,26 +33,20 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(P,A){
-  let isTrue = false
- for(let i = 0;i<P;i++){
-  if(A%i===0){
-    let l = i
-    let b = A/i
-    // Area ku formula Area = l*b //inside the rectangle
-    if(2*(l+b)===P){     //Perimeter = 2*(l+b) //outside sides 
-      // console.log(l,b)
-      isTrue = true
-    }
+function result(str,N){
+  let a = str.split('')
+  for(let i = 0 ; i<N;i++){
+    let first = a.shift()
+    a.push(first)
   }
-}
-console.log(isTrue?"yes":"no")
+  console.log(a.join(''))
  }
 
 
 
 rl.on("close", () => {
- const [P,A] = userInput[0].split(' ').map(n=>Number(n))
+ const [str,n] = userInput[0].split(' ')
+ let N = Number(n)
 
- result(P,A)
+ result(str,N)
 });
