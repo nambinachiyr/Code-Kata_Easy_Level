82c7b46d3bb1bf732ae8a57329c970552cc33bd5
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-16 17:24:50 +0530

    111. Bitwise AND of Array Elements

--- OES.js
+++ OES.js
@@ -1,23 +1,28 @@
 /*
-110. Divisibility Check of a Number
+111. Bitwise AND of Array Elements
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a number N, check if N is divisible by any number less than N (ie.,it leaves no remainder)except 1.
+Given a number N and an array of N elements ,find the Bitwise AND of the array elements.
 
 
 Input Description:
-Input Size : 1 <= N <= 100000
+The input consists of an integer N, representing the size of the array, followed by N array elements. N <= 100000.
+
+
+Output Description:
+The output is the Bitwise AND of all elements in the array.
 
 
 Sample Input:
-10
+4
+4 3 2 1
 
 
 Sample Output:
-yes
+0
 
 */
 
@@ -33,19 +38,24 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(n){
-  let isDivisible = false
- for(let i = 2;i<n;i++){
-  let divider = n/i
-  if(Number.isInteger(divider)){
-      isDivisible = true
-      break
-  }
+function result(n,arr){
+  let preValue;
+ for(let i= 0;i<arr.length;i++){
+   if(i!==arr.length-1){
+    if(!preValue){
+      preValue = arr[i]&arr[i+1]
+      console.log(preValue)
+    }
+    let now = arr[i]&arr[i+1]
+    console.log(preValue,now)
+     preValue = preValue&now
+   }
  }
- console.log(isDivisible?"yes":"no")
+ console.log(preValue,"pr")
 }
 rl.on("close", () => {
  const n = Number(userInput[0])
+ const arr = userInput[1].split(' ').map(n=>Number(n))
 
- result(n)
+ result(n,arr)
 });
