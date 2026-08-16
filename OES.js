6d514e60d2a6501fcf2a7a618bda417791d263c4
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-17 00:15:36 +0530

    124. Kth Digit from Position

--- OES.js
+++ OES.js
@@ -1,19 +1,19 @@
 /*
-123. Repeating Digits Check
+124. Kth Digit from Position
 
-Geekoin40
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Given a number N,check whether it has repeating digits in it.print 'yes' if it has repeating digits otherwise print 'no'.
+Given a number N, print the kth digit from the given position p(given order N P K).
 
 
 Sample Input:
-11234
+5765 2 1
 
 
 Sample Output:
-yes
+6
 */
 
 const readline = require('readline')
@@ -28,25 +28,24 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(nums){
+function result(N,P,K){
 
-  let isThere = false
-  for(let i = 0;i<nums.length;i++ ){
-    for(let j = i;j<nums.length;j++){
-
-      if(i!==nums.length){
-        if(nums[i]===nums[j+1]){
-          isThere = true
-          break
-        }
-      }
-    }
-  }
- console.log(isThere?"yes":"no")
+  // P->position of Kth element mean p is 1 <- This is position not Index so we did P-1
+// for to get the Index so it is 5 then the Kth element so k = 3 remove till position 1
+// and count 3 element the 3 element is nswer 5 is position so remove it 5-1,6-2,7-3
+let answer = 0 
+for(let i = 1;i<N.length;i++){
+  answer = N[(P-1)+K]
+ }
+ console.log(answer)
 }
 rl.on("close", () => {
 //  const n = Number(userInput[0])
- const nums = userInput[0].split('').map(n=>Number(n))
+ const [nums,p,k] = userInput[0].split(' ')
+ const N = nums.split('').map(n=>Number(n))
+const P = Number(p)
+const K = Number(k)
+
  
- result(nums)
+ result(N,P,K)
 });
