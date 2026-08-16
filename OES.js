b63fa82b1f9642d952a46011211d6825771c2136
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-17 00:22:56 +0530

    125. Sum of First and Last Digits

--- OES.js
+++ OES.js
@@ -1,19 +1,23 @@
 /*
-124. Kth Digit from Position
+125. Sum of First and Last Digits
 
 Geekoin50
 Medium
 Topics
 Problem Statement:
-Given a number N, print the kth digit from the given position p(given order N P K).
+Given a number N, print the sum of its first and last digit.
+
+
+Input Description:
+Input Size : |N| <= 10000
 
 
 Sample Input:
-5765 2 1
+51233
 
 
 Sample Output:
-6
+8
 */
 
 const readline = require('readline')
@@ -28,24 +32,11 @@ rl.on("line", (data) => {
   userInput.push(data.trim());
 });
 
-function result(N,P,K){
-
-  // P->position of Kth element mean p is 1 <- This is position not Index so we did P-1
-// for to get the Index so it is 5 then the Kth element so k = 3 remove till position 1
-// and count 3 element the 3 element is nswer 5 is position so remove it 5-1,6-2,7-3
-let answer = 0 
-for(let i = 1;i<N.length;i++){
-  answer = N[(P-1)+K]
- }
- console.log(answer)
+function result(N){
+  let sum = N[0]+N[N.length-1]
+  console.log(sum)
 }
 rl.on("close", () => {
-//  const n = Number(userInput[0])
- const [nums,p,k] = userInput[0].split(' ')
- const N = nums.split('').map(n=>Number(n))
-const P = Number(p)
-const K = Number(k)
-
- 
- result(N,P,K)
+ const N = userInput[0].split('').map(n=>Number(n))
+ result(N)
 });
