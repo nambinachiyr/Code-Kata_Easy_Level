8048942506cf11007092b55aa1af5d8cdfb06fe3
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-17 10:34:42 +0530

    137. Prefix Sum Array

--- OES.js
+++ OES.js
@@ -1,28 +1,28 @@
 /*
-136. Word Position in String
+137. Prefix Sum Array
 
 Geekoin50
 Medium
 Topics
 Problem Statement:
-Given 2 strings S and X print the word position of X in S.(word count starts from 1).If the given word doesn't exists in S print '-1'.
+Given a number N and an array of N elements, print the prefix sum array.
 
 
 Input Description:
-The input consists of 2 strings S and X. The size of S and X are between 1 and 1000 characters (1 <= |s|, |x| <= 1000).
+Input Size : N <= 100000
 
 
 Output Description:
-The output is the word position of X in S (starting from 1), or -1 if X is not found.
+The output is the prefix sum array.
 
 
 Sample Input:
-codekata coding challenge
-coding
+4
+2 4 4 2
 
 
 Sample Output:
-2
+2 6 10 12
 */
 
 const readline = require('readline');
@@ -37,39 +37,16 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-  function result(string,word) {
-  let isWord = false
-  let index;
-  for(let j=0;j<string.length;j++){
-    let count =1
-    for(let i = 0 ;i<string[j].length;i++){
-      if(string[j][i]===word[i]){
-        isWord = true
-        index = j
-        // console.log(count,string[j].length)
-        if(count===string[j].length){
-          break
-        }
-        count++
-        
-      }
-      else{
-        isWord = false
-        count=0
-        break
-      }
-    }
-    if(count!==string[j] && !isWord){
-
-    }else{
-      break
-    }
+  function result(n,nums) {
+  for(let i =0;i<n-1;i++){
+    nums[i+1] = nums[i+1]+nums[i]
   }
-  console.log(isWord?index+1:-1)
+  console.log(nums.join(' '))
 }
 
 rl.on('close', () => {
- const string = userInput[0].split(' ')
- const word = userInput[1].split('')
- result(string,word)
+const n = Number(userInput[0])
+const nums = userInput[1].split(' ').map(n=>Number(n))
+// console.log(n,nums)
+ result(n,nums)
 });
