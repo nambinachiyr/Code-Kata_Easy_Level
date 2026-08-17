8a0c73ea6d08ee156614fd67ab6dcbd33743e819
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-17 07:29:08 +0530

    131. Max Sum of Consecutive Pairs

--- OES.js
+++ OES.js
@@ -38,31 +38,49 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-function result(arr) {
-  let max = 0;
+// function result(arr) {
 
-  function maxNumInPairs(num1, num2) {
-    console.log(num1, num2);
-    if (num1 > num2) {
-      max = num1 + max;
-      // console.log(max,"NUM1",num1)
-    } else {
-      max = num2 + max;
-      // console.log(max,"NUM2",num2)
+  // 3/5
+  // let max = 0;
+
+  // function maxNumInPairs(num1, num2) {
+  //   console.log(num1, num2);
+  //   if (num1 > num2) {
+  //     max = num1 + max;
+  //     // console.log(max,"NUM1",num1)
+  //   } else {
+  //     max = num2 + max;
+  //     // console.log(max,"NUM2",num2)
+  //   }
+  // }
+  // for (let i = 1; i < arr.length - 1; i++) {
+  //   let temp = arr[i + 1];
+  //   arr[i + 1] = arr[-i-1];
+  //   arr[-1- i] = temp;
+  // }
+  // console.log(arr);
+  // for (let i = 0; i < arr.length - 1; i++) {
+  //   maxNumInPairs(arr[i], arr[i + 1]);
+  // }
+
+  // console.log(max);
+// }
+// chatgpt
+  function result(arr) {
+  arr.sort((a, b) => b - a)
+
+  let sum = 0
+
+  if (arr.length % 2 !== 0) {
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+      sum += arr[i] * 2
     }
-  }
-  for (let i = 1; i < arr.length - 1; i++) {
-    let temp = arr[i + 1];
-    arr[i + 1] = arr[-i-1];
-    arr[-1- i] = temp;
-  }
-  console.log(arr);
-  for (let i = 0; i < arr.length - 1; i++) {
-    maxNumInPairs(arr[i], arr[i + 1]);
-  }
+  
 
-  console.log(max);
+  console.log(sum)
 }
+}
+
 rl.on('close', () => {
   const n = userInput[0];
   const arr = userInput[1].split(' ').map((n) => Number(n));
