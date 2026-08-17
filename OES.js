4dfabd5914ee7e73eb04b5ad67aa4167c2c89f7f
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-17 08:14:16 +0530

    133. Sum of Consecutive Pairs

--- OES.js
+++ OES.js
@@ -1,20 +1,19 @@
 /*
-Problem
-Hints & Solutions
-Submissions
+133. Sum of Consecutive Pairs
 
-
-131. Max Sum of Consecutive Pairs
-
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Given a number N and an array of N integers, find the maximum sum of the maximum elements obtained by considering all consecutive pairs of adjacent elements after rearranging the array suitably.
+Given a number N and an array of N elements, find the sum of the sums obtained by considering all consecutive pairs of adjacent elements.
 
 
 Input Description:
-The input consists of a single integer N, followed by N integers representing the elements of the array. N is an integer such that N <= 100000.
+The input consists of a number N and an array of N elements. N <= 100000.
+
+
+Output Description:
+The output is the sum of the sums obtained by considering all consecutive pairs of adjacent elements.
 
 
 Sample Input:
@@ -23,7 +22,8 @@ Sample Input:
 
 
 Sample Output:
-18
+24((1+2)+(2+3)+(3+4)+(4+5))
+
 */
 
 const readline = require('readline');
@@ -38,47 +38,19 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-// function result(arr) {
-
-  // 3/5
-  // let max = 0;
-
-  // function maxNumInPairs(num1, num2) {
-  //   console.log(num1, num2);
-  //   if (num1 > num2) {
-  //     max = num1 + max;
-  //     // console.log(max,"NUM1",num1)
-  //   } else {
-  //     max = num2 + max;
-  //     // console.log(max,"NUM2",num2)
-  //   }
-  // }
-  // for (let i = 1; i < arr.length - 1; i++) {
-  //   let temp = arr[i + 1];
-  //   arr[i + 1] = arr[-i-1];
-  //   arr[-1- i] = temp;
-  // }
-  // console.log(arr);
-  // for (let i = 0; i < arr.length - 1; i++) {
-  //   maxNumInPairs(arr[i], arr[i + 1]);
-  // }
-
-  // console.log(max);
-// }
-// chatgpt
   function result(arr) {
-  arr.sort((a, b) => b - a)
+  // arr = arr.sort((a,b)=>a-b)
+  // console.log(arr)
 
-  let sum = 0
+  let max=0
+  function maxOfPairs(n1,n2){
+   max=max+n1+n2
+  }
 
-  if (arr.length % 2 !== 0) {
-    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-      sum += arr[i] * 2
-    }
-  
-
-  console.log(sum)
-}
+  for(let i =0 ;i<arr.length-1;i++){
+   maxOfPairs(arr[i],arr[i+1])
+  }
+  console.log(max)
 }
 
 rl.on('close', () => {
