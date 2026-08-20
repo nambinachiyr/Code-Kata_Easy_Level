e3ad4480f35a381d38994505cdb36eaeefef383b
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-20 15:15:14 +0530

    763. Sum of Two Numbers-2

--- OES.js
+++ OES.js
@@ -1,22 +1,33 @@
 /*
-160. Reachable Point
+763. Sum of Two Numbers-2
 
-Geekoin40
-Medium
+Geekoin10
+Easy
 Topics
 Problem Statement:
-Given 2 numbers n and m, n pairs of numbers a and b are given. In each pair 'a' means a person can start moving from point 'a' in the x axis to point 'b'(he can visit any point in between). Find if he can visit point m.
+You are given Two Numbers, A and B. If C = A + B. Find C. Note: Round off the output to a single decimal place.
+
+
+Input Description:
+You are provided with two numbers A and B.
+
+
+Output Description:
+Find the sum of the two numbers (A + B)
+
+
+Explanation:
+1+1 = 2
 
 
 Sample Input:
-3 5
-0 2
-2 4
-3 5
+1
+1
 
 
 Sample Output:
-yes
+2
+
 */
 
 const readline = require('readline');
@@ -31,31 +42,15 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-
- function result(n,m,pairs){
-   let isBetween = false
-   for(let i=0;i<n;i++){
-     p = pairs[i].split(' ').map(n=>Number(n))
-    //  console.log(p,"p")
-      for(let j = 0;j<1;j++){
-        // console.log(p[j],p[j+1])
-         if(p[j]<=m && m<=p[j+1]){
-          isBetween = true
-         }else{
-          // console.log("no")
-          isBetween = false
-         }
-      }
-    }
-    console.log(isBetween?"yes":"no")
-   
- }
-  
+ function result(nums){
+    
+  const sum = nums.reduce((acc,cur)=>acc+cur,0);
+  console.log(sum)
+ }  
 
 rl.on('close', () => {  
-  const [n,m] = userInput[0].split(' ').map(n=>Number(n))
-  const pairs = userInput.slice(1)
-  
+
+  const nums = userInput.map(n=>Number(n))  
  
-result(n,m,pairs)
+result(nums)
 });
