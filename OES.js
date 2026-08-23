5dbb1e47d5f16f1a7e3adeccedea70b9185226bf
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-23 18:33:31 +0530

    873. Number Half Pyramid Pattern

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-872. Half Diamond Star Number Pattern
+873. Number Half Pyramid Pattern
 
-Geekoin50
-Medium
+Geekoin30
+Easy
 Topics
 Problem Statement:
-Generate a half diamond pattern using stars and numbers in a palindromic pattern.
+Generate a half pyramid pattern using numbers.
 
 
 Input Description:
@@ -13,27 +13,25 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the half diamond pattern using stars and numbers in a palindromic pattern based on the given integer R.
+Print the half pyramid pattern based on the given integer R.
 
 
 Explanation:
-From the given input R=3, print the half diamond pattern with number and star pattern in the form of palindromic number.
+From the given input R=5, print the half pyramid number pattern upto R row.
 
 
 Sample Input:
-3
+5
 
 
 Sample Output:
 
 
-*  
-*1*  
-*121*  
-*12321*  
-*121*  
-*1*  
-*
+1  
+12  
+123  
+1234  
+12345  
 
 
 */
@@ -52,33 +50,16 @@ rl.on('line', (data) => {
 
  function result(nums){
  
-  for(let row = 0 ;row<=nums;row++){
-    let str = '*'
+  for(let row = 1 ;row<=nums;row++){
+    let str = ''
     for(let i = 1;i<=row;i++){
       str +=i 
     }
-    for(let i = row-1;i>0;i--){
-      str+=i
-    }
-    if(row!==0){
-      str=str+'*'
-    }
+    
     console.log(str)
   }
    
-  for(let row = nums-1 ;row>=0;row--){
-    let str = '*'
-    for(let i = 1;i<=row;i++){
-      str +=i 
-    }
-    for(let i = row-1;i>0;i--){
-      str+=i
-    }
-    if(row!==0){
-      str=str+'*'
-    }
-    console.log(str)
-  }
+  
   }
  
 
