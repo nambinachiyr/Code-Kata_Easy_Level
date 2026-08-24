99cc86cd5597130f08b4ebcd81be673ff10c901c
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 19:39:38 +0530

    902. Left Arrow Star Pattern

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-901. Butterfly Pattern with Stars
+902. Left Arrow Star Pattern
 
-Geekoin60
+Geekoin50
 Medium
 Topics
 Problem Statement:
-Write a code to generate a butterfly pattern printing using stars.
+Write a code to generate a left arrow pattern using stars.
 
 
 Input Description:
@@ -13,11 +13,11 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the butterfly pattern printing using stars based on the given integer R.
+Print the left arrow pattern based on the given integer R.
 
 
 Explanation:
-From the given input R= 5, print the R*2 rows in the form of butterfly using stars.
+From the given input R=5,print the left arrow using stars
 
 
 Sample Input:
@@ -27,16 +27,15 @@ Sample Input:
 Sample Output:
 
 
-*        *  
-**      **  
-***    ***  
-****  ****  
-**********  
-**********  
-****  ****  
-***    ***  
-**      **  
-*        *  
+*  
+ *  
+  *  
+   *  
+*****  
+   *  
+  *  
+ *  
+*  
 
 */
 
@@ -52,45 +51,49 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
- function result(n){
+function result(n) {
+  for (let row = 1; row <= n; row++) {
+    let str = '';
 
-   for(let row =1; row<=n; row++){
-     let str = ''
-     
-     //  Top
-     for(let leftSide=1; leftSide<=row; leftSide++){     
-       str+='*'
+    //  Top
+    for (let space = 1; space < row; space++) {
+      if (row !== n) {
+        str += ' ';
+      } else {
+        str += '';
       }
-     for(let space =row*2;space<=n*2-1;space++){
-      str+=' '
-     }
-     for(let rightSide=1;rightSide<=row;rightSide++){
-      str+='*'
-     }
-    console.log(str)
+    }
+    for (let leftSide = 1; leftSide <= n; leftSide++) {
+      if (row !== n) {
+        str += '*';
+        break;
+      } else {
+        str += '*';
+      }
+    }
+
+    console.log(str);
 
     //  Bottom
-  }  
-  for(let row=1;row<=n;row++){
-    let str=''
-    for(let leftSide=row; leftSide<=n; leftSide++){     
-   str+='*'
- }
- for(let space =2;space<=row+row-1;space++){
-  str+=' '
- }
- for(let rightSide=row;rightSide<=n;rightSide++){
-  str+='*'
- }   
-console.log(str)
   }
+  for (let row = 1; row <= n; row++) {
+    let str = '';
+    for (let space =2; space <=n-row; space++) {
+      str += ' ';
+    }
+    for (let rightSide =1; rightSide <= 1; rightSide++) {
+      if(row!==n){
+        str += '*';
+      }else{
+        str+=''
+      }
+    }
+    console.log(str);
   }
- 
+}
 
-rl.on('close', () => {  
+rl.on('close', () => {
+  const n = Number(userInput[0]);
 
-  const n = Number(userInput[0])
- 
-  result(n)
-
+  result(n);
 });
