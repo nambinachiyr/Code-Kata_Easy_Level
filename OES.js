10a7883c9f7186aa8f9ec4105cba5c0ba1ba55d5
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-25 01:08:31 +0530

    919. Matrix Number Pattern

--- OES.js
+++ OES.js
@@ -1,23 +1,23 @@
 /*
-918. Number Square Pattern-2
+919. Matrix Number Pattern
 
-Geekoin50
+Geekoin40
 Medium
 Topics
 Problem Statement:
-Write a code to generate a square pattern using numbers.
+Write a code to generate a matrix number pattern.
 
 
 Input Description:
-Given an integer R indicates number of R*2-1 rows.Where 1<=R<=100
+Given an odd integer R indicates number of rows.R is always odd integer. Where 1<=R<=100
 
 
 Output Description:
-Print the square pyramid number based on the given integer R.
+Print the matrix number pattern R*R based on the given integer R.
 
 
 Explanation:
-Form the input R=5, print 1 to R in first column (vertical) and print 1 in first column ,next 4(22),9(33),16(4(4)),25(5(5)).
+From the given input R=5, print the square matrix R*R.
 
 
 Sample Input:
@@ -26,16 +26,11 @@ Sample Input:
 
 Sample Output:
 
-
-1  
-1 4  
-1 4 9  
-1 4 9 16  
-1 4 9 16 25  
-1 4 9 16  
-1 4 9  
-1 4  
-1  
+33333  
+32223  
+32123  
+32223  
+33333  
 
 */
 
@@ -52,21 +47,18 @@ rl.on('line', (data) => {
 });
 
 function result(n) {
+ 
+  let num = Math.round(n/2)
   for(let row = 1; row<=n; row++){
-    let str =''
-    for(let j = 1; j<=row; j++){
-     str+=j*j+" "
-    }
-    console.log(str.trim())
-  }
-
-  for(let row = n-1; row>=1; row--){
     let str = ''
-    for(let j = 1;j<=row; j++){
-      str+=j*j+' '
+    for(let j = 1; j<=n; j++){
+      let min = Math.min(row,j,n-row+1,n-j+1) //(1,1,5-1+1,5-1+1) => min=(1)
+      let layer = num-min+1 //3-1+1=>3
+      str+=layer
     }
-    console.log(str.trim())
+    console.log(str)
   }
+ 
 }
 
 rl.on('close', () => {
