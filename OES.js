db986ba2bf5fc5681198bccfcfbeca3ef639d758
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 22:45:43 +0530

    916. Alphabet Pyramid Pattern-6

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-915. Alphabet Half Pyramid Pattern-3
+916. Alphabet Pyramid Pattern-6
 
 Geekoin40
 Medium
 Topics
 Problem Statement:
-Write a code to generate a aplhabet half pyramid pattern.
+Write a code to generate a triangle character pattern.
 
 
 Input Description:
@@ -13,11 +13,11 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the alphabet half pyramid pattern according to the given integer R.
+Print the alphabet pyramid pattern according to the given integer R.
 
 
 Explanation:
-From the given input R=5,print 5 rows of the pyramid alphabet pattern.
+From the given input R=5,print 5 rows of the pyramid alphabet pattern.First line print A,then next line print A B, repeat this process until reach R characters on last line.
 
 
 Sample Input:
@@ -26,12 +26,11 @@ Sample Input:
 
 Sample Output:
 
-
-EDCBA  
-EDCB  
-EDC  
-ED  
-E  
+    A  
+   A B  
+  A B C  
+ A B C D  
+A B C D E  
 
 */
 
@@ -50,8 +49,15 @@ rl.on('line', (data) => {
 function result(n) {
   for (let row = 1; row<=n; row++) {
     let str = '';
-   for(let letter=n;letter>=row;letter--){
-      str+= String.fromCharCode(letter+64)
+    for(let space = row;space<n;space++){
+      str+=' '
+    }
+   for(let letter=1;letter<=row;letter++){
+      if(letter!==row){
+        str+= String.fromCharCode(letter+64)+' '
+      }else{
+        str+= String.fromCharCode(letter+64)
+      }
    }
 
    console.log(str)
