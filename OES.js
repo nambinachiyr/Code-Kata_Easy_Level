66fe998979011be88f5972977a2be02f9df1a2d5
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 21:50:09 +0530

    910. Alphabet Pyramid Pattern-3

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-909. Number Pyramid Pattern-4
+910. Alphabet Pyramid Pattern-3
 
 Geekoin50
 Medium
 Topics
 Problem Statement:
-Write a code to generate a pyramid pattern on numbers.
+Write a code to generate a alphabet pyramid pattern.
 
 
 Input Description:
@@ -13,11 +13,11 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the pyramid number pattern based on the given integer R.
+Print the alphabet pyramid pattern according to the given integer R.
 
 
 Explanation:
-From the given input R=5, print the 5 rows of pyramid pattern using numbers.
+From the given input R=5,print 5 rows of the pyramid alphabet.
 
 
 Sample Input:
@@ -27,13 +27,11 @@ Sample Input:
 Sample Output:
 
 
-    0  
-   101  
-  21012  
- 3210123  
-432101234  
-
-
+    A  
+   ABC  
+  ABCDE  
+ ABCDEFG  
+ABCDEFGHI  
 
 */
 
@@ -56,12 +54,10 @@ function result(n) {
     for (let space = row; space <n-1; space++) {
       str += ' ';
     }
-    for (let leftSide = row; leftSide >=0; leftSide--) {
-      str += leftSide;
+    for (let leftSide = 0; leftSide<=row*2; leftSide++) {
+      str += String.fromCharCode(leftSide+65);
     }
-    for (let rightSide = 1; rightSide<=row; rightSide++) {
-      str += rightSide;
-    }
+   
 
     console.log(str);
   }
