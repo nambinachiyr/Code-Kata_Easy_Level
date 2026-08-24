94f90bac840cf37292ae7f0fd6866dd38894858d
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 18:40:13 +0530

    897. Number Square Pattern

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-896. Number Half Pyramid Pattern-8
+897. Number Square Pattern
 
 Geekoin50
 Medium
 Topics
 Problem Statement:
-Write a code to generate a half pyramid pattern using numbers.
+Write a code to generate a square pattern using numbers.
 
 
 Input Description:
@@ -13,11 +13,13 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the number half pyramid pattern with the size R.
+Print the number pattern separated with space with the size R.
 
 
 Explanation:
-From the given input R=5, print even and odd numbers alternative rows.
+From the given input R=5, print 5 in 5 times with spaces,
+next line print 4 5 5 5 5,and then 3 4 5 5 5,then 2 3 4 5 5
+finally print form 1 to 5(R).
 
 
 Sample Input:
@@ -27,11 +29,11 @@ Sample Input:
 Sample Output:
 
 
-1  
-24  
-135  
-2468  
-13579  
+5 5 5 5 5  
+4 5 5 5 5  
+3 4 5 5 5  
+2 3 4 5 5  
+1 2 3 4 5  
 
 */
 
@@ -49,21 +51,25 @@ rl.on('line', (data) => {
 
  function result(n){
 
-  for(let row =1; row<=n; row++){
-    let str = ''
-   
-   for(let j=1; j<=row*2; j++){
-  if(row % 2 !==0){
-    if(j%2!==0){
-      str+=j
-    }
-  }else{
-    if(j%2===0){
-      str+=j
-    }
-  }
-   
+   for(let row =n; row>=1; row--){
+     let str = ''
+     
+     for(let j=row; j<=n+row-1; j++){     
+      if(j>n){
+       if(j===n+row-1){
+         str+=n
+       }else{
+        str+=n+' '
+       }
+      }else{
+        if(j===n+row-1){
+         str+=j
+       }else{
+        str+=j+' '
+       }
+      }
    }
+  
    
     console.log(str)
   }  
