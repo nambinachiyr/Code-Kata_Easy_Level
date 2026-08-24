9ba4fef96f5ad322efcbd1480e086e695b514139
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 19:55:58 +0530

    904. Star Pyramid Pattern-3

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-903. Right Arrow Pattern
+904. Star Pyramid Pattern-3
 
-Geekoin50
-Medium
+Geekoin30
+Easy
 Topics
 Problem Statement:
-Write a code to generate a right arrow using patterns.
+Write a code to generate a pyramid using stars.
 
 
 Input Description:
@@ -13,26 +13,26 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the right arrow pattern using stars based on the given integer R.
+Print the pyramid pattern using stars based on the given integer R.
+
+
+Explanation:
+From the given input R=6, to print the half pyramid using star with the given rows 6
 
 
 Sample Input:
-5
+6
 
 
 Sample Output:
 
 
-    *  
-   *  
-  *  
- *  
-*****  
- *  
-  *  
-   *  
-    *  
-
+     *  
+    **  
+   ***  
+  ****  
+ *****  
+******  
 */
 
 const readline = require('readline');
@@ -52,40 +52,17 @@ function result(n) {
   for (let row = 1; row <= n; row++) {
     let str = '';
 
-    //  Top
     for (let space = row; space < n; space++) {
-      if (row !== n) {
-        str += ' ';
-      }
-    }
-    for (let leftSide = 1; leftSide <= n; leftSide++) {
-      if (row !== n) {
-        str += '*';
-        break;
-      } else {
-        str += '*';
-      }
-    }
-
-    console.log(str);
-
-    //  Bottom
-  }
-  for (let row = 1; row <= n; row++) {
-    let str = '';
-    for (let space =1; space <=row; space++) {
-      if(row!==n){
-        str += ' ';
-      }
-    }
-    for (let rightSide =row; rightSide < n; rightSide++) {
+      str += ' ';
       
-        str += '*';
-        break
-    
     }
+    for (let leftSide = 1; leftSide <= row; leftSide++) {
+     str+='*'
+    }
+
     console.log(str);
   }
+  
 }
 
 rl.on('close', () => {
