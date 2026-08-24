5d3dfd864e948aa79ec1d8c3eb8fe5eb6e76970f
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-24 18:47:47 +0530

    898. Rhombus Star Pattern

--- OES.js
+++ OES.js
@@ -1,11 +1,11 @@
 /*
-897. Number Square Pattern
+898. Rhombus Star Pattern
 
 Geekoin50
 Medium
 Topics
 Problem Statement:
-Write a code to generate a square pattern using numbers.
+Write a code to generate an Rhombus Pattern using stars.
 
 
 Input Description:
@@ -13,28 +13,24 @@ Given an integer R indicates number of rows.Where 1<=R<=100
 
 
 Output Description:
-Print the number pattern separated with space with the size R.
+Print the solid rhombus using stars with the size of rhombus R.
 
 
 Explanation:
-From the given input R=5, print 5 in 5 times with spaces,
-next line print 4 5 5 5 5,and then 3 4 5 5 5,then 2 3 4 5 5
-finally print form 1 to 5(R).
+From the given input R=4 print the rhombus 4 * 4 using stars.
 
 
 Sample Input:
-5
+4
 
 
 Sample Output:
 
 
-5 5 5 5 5  
-4 5 5 5 5  
-3 4 5 5 5  
-2 3 4 5 5  
-1 2 3 4 5  
-
+   ****  
+  ****  
+ ****  
+****  
 */
 
 const readline = require('readline');
@@ -51,23 +47,15 @@ rl.on('line', (data) => {
 
  function result(n){
 
-   for(let row =n; row>=1; row--){
+   for(let row =1; row<=n; row++){
      let str = ''
      
-     for(let j=row; j<=n+row-1; j++){     
-      if(j>n){
-       if(j===n+row-1){
-         str+=n
-       }else{
-        str+=n+' '
-       }
-      }else{
-        if(j===n+row-1){
-         str+=j
-       }else{
-        str+=j+' '
-       }
-      }
+     for(let space = 1;space<=n-row;space++){
+      str+=' '
+     }
+
+     for(let j=1; j<=n; j++){     
+     str+='*'
    }
   
    
