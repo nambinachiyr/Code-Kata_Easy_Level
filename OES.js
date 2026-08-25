5059db7099889345902468a384dadda812f7a417
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-25 18:01:34 +0530

    945. LCM without GCD

--- OES.js
+++ OES.js
@@ -1,31 +1,32 @@
 /*
-941. Print Integer Digits
+945. LCM without GCD
 
-Geekoin30
-Easy
+Geekoin80
+Hard
 Topics
 Problem Statement:
-Write a code to get an integer N and print the digits of the integer.
+Write a program to get a list of integers as input and find the LCM of the values without using GCD
 
 
 Input Description:
-A single line contains an integer N.
+First line contains an integer N, number of values.
+Second line contains N space separated values.
 
 
 Output Description:
-Print the digits of the integer in a single line separated by space,
+Print the LCM of the values.
 
 
 Explanation:
-The digits are splitted and displayed.
+The LCM of the 1,2,3,4,5 is 60
 
 
 Sample Input:
-348
+1 2 3 4 5
 
 
 Sample Output:
-3 4 8
+60
 
 */
 
@@ -41,12 +42,33 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-function result(n) {
+function result(a) {
+  const max = Math.max(...a)
+  let i = max
+
+  while(true){
+  //  This Condition will be always true until we found the tracate
+    let isLCM = true
+   for(let num =0 ;num<a.length;num++){
+    // Inside the array search for it's divided the max value
+    if(!Number.isInteger(i/a[num])){
+      // Check if is divide  is the Number is Integer or not
+      // console.log(i/a[num])
+      isLCM =false
+       break
+    }
+  }
+  if(isLCM){
+    break
+  }
+  i+=max
+    // console.log(i,"o")
+  }
+  console.log(i)
 }
 
 rl.on('close', () => {
-  const n = userInput[0].split('').join(' ');
-  console.log(n)
-
-  // result(n);
+  const a = userInput[0].split(' ').map(n=>Number(n));
+ 
+  result(a);
 });
