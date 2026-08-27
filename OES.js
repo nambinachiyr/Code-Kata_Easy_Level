3611b759b65b31f848e2ab7b2ede947a2a65fd40
Nambi Nachiyar <nambi2731@gmail.com>
2026-08-27 10:28:26 +0530

    279. Find the Single Number

--- OES.js
+++ OES.js
@@ -1,24 +1,28 @@
 /*
-253. Nearest Neighbors in Array
+279. Find the Single Number
 
 Geekoin60
 Medium
 Topics
 Problem Statement:
-Given two numbers N,K and an array of size N, print the three nearest neighbours of K(nearest neighbours are numbers which have least difference with K).
+Given a number N followed by an array of N integers, every element appears twice except for one. Find that single one and print it.
 
 
 Input Description:
-Input Size : 4 <= N, K <= 100000
+The input consists of a number N followed by an array of N integers. N is the size of the array, where N <= 100000.
+
+
+Output Description:
+The output is the single integer that appears only once in the array.
 
 
 Sample Input:
-5 3
-1 2 3 4 6
+5
+30 5 5 30 -45
 
 
 Sample Output:
-2 4 1
+-45
 */
 
 const readline = require('readline');
@@ -33,28 +37,23 @@ rl.on('line', (data) => {
   userInput.push(data.trim());
 });
 
-function result(num,k,arr) {
-  const filter = arr.filter(n=>n!==k)
-  let diff_k = []
-  for(let i=0; i<filter.length; i++){
-    let diff = Math.abs(k-filter[i]) // This function Using for Cacle the Negative Number
-    diff_k.push({
-      value:filter[i],
-      dif:diff
-    })
+function result(num,arr) {
+  for(let i =0 ;i<num;i++){
+    let count = 0
+  for(let j=0; j<num;j++){
+    if(arr[i]===arr[j]){
+      count=count+1
+    }
   }
-  diff_k.sort((a,b)=>a.dif-b.dif)
+  if(count===1){
+    console.log(arr[i])
+  }
+ }
 
-  let nearestNumber = []
-  for(let i=0; i<3; i++){
-    nearestNumber.push(diff_k[i].value)
-  }
-  console.log(nearestNumber.join(' '))
- 
 }
 
 rl.on('close', () => {
-  const [num,k] = userInput[0].split(' ').map(n=>Number(n));
+  const [num] = userInput[0].split(' ').map(n=>Number(n));
   const arr = userInput[1].split(' ').map(n=>Number(n))
-  result(num,k,arr);
+  result(num,arr);
 });
